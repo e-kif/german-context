f5a5b40778ea58a69141c529672b6c2043884b1f
e-kif <e.kifiuk@gmail.com>
2025-02-04 20:14:07 +0200

    docs for user_words endpoints

--- routers/words.py
+++ routers/words.py
@@ -51,11 +51,12 @@ async def suggest_words_by_letter_combination(
     return suggest_words
 
 
-@words.get('/{user_word_id}')
+@words.get('/{user_word_id}', summary="Show user word's info")
 async def get_own_word(
         current_user: Annotated[UserOut, Depends(get_current_active_user)],
         user_word_id: Annotated[int, Path(title='UserWord id', ge=1)]
 ) -> WordOut:
+    """## Given a user_word_id returns the user word's info"""
     db_word = db_manager.get_user_word_by_id(user_word_id)
     check_for_exception(db_word, 404)
     if db_word.user_id != current_user.id:
@@ -63,11 +64,17 @@ async def get_own_word(
     return serialization.word_out_from_user_word(db_word)
 
 
-@words.post('')
+@words.post('', summary='Add a user word')
 async def add_user_word(
         current_user: Annotated[UserOut, Depends(get_current_active_user)],
         word: UserWordIn
 ) -> WordOut:
+    """## Adds new word to user's words
+    Word can be added:
+    1. by providing only one german word (all info will be fetched automatically).
+    2. by providing *word*, *translation*, *level* and *word_type*.
+
+    """
     parsed_word = get_word_info(word.word)
     custom_word = False
     if isinstance(parsed_word, str) and not all([word.english, word.level, word.word_type]):
@@ -110,11 +117,14 @@ async def add_user_word(
     return serialization.word_out_from_user_word(db_user_word)
 
 
-@words.delete('/{user_word_id}')
+@words.delete('/{user_word_id}', summary="Removes user's word from the app")
 async def remove_user_word(
         current_user: Annotated[UserOut, Depends(get_current_active_user)],
         user_word_id: Annotated[int, Path(ge=1)]
 ) -> WordOut | None:
+    """## Given *user_word_id* removes it from the app
+    This action is irreversible.
+    """
     the_word = db_manager.get_user_word_by_id(user_word_id)
     check_for_exception(the_word, 404)
     if current_user.id != the_word.user_id:
@@ -124,10 +134,20 @@ async def remove_user_word(
     return serialized_word
 
 
-@words.patch('/{user_word_id}')
+@words.patch('/{user_word_id}', summary="Update user word's info")
 async def patch_own_word(user_word_id: Annotated[int, Path(ge=1)],
                          current_user: Annotated[UserOut, Depends(get_current_active_user)],
                          word: UserWordPatch) -> WordOut:
+    """## Update info for user word with *user_word_id*
+    Expected at least one of the parameters:
+     - word
+     - word_type
+     - english
+     - level
+     - topics
+     - example
+     - example_translation
+     """
     db_user_word = db_manager.get_user_word_by_id(user_word_id)
     check_for_exception(db_user_word, 404)
     if db_user_word.user_id != current_user.id:
@@ -151,10 +171,12 @@ async def patch_own_word(user_word_id: Annotated[int, Path(ge=1)],
     return serialization.word_out_from_user_word(updated_db_user_word)
 
 
-@words.put('/{user_word_id}')
+@words.put('/{user_word_id}', summary="Update user word's info")
 async def update_own_word(user_word_id: Annotated[int, Path(ge=1)],
                           current_user: Annotated[UserOut, Depends(get_current_active_user)],
                           word: UserWordIn) -> WordOut:
+    """## Update info for user word with *user_word_id*
+    All parameters are required."""
     db_user_word = db_manager.get_user_word_by_id(user_word_id)
     check_for_exception(db_user_word, 404)
     if db_user_word.user_id != current_user.id:
