0d03401f68a80e40abf47355f77f866e9b815d94
e-kif <e.kifiuk@gmail.com>
2025-03-06 14:31:20 +0200

    test fixture init

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,16 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from main import app
 
 
-def test_read_home():
-    client = TestClient(app)
+@pytest.fixture(scope='module')
+def client():
+    with TestClient(app) as client:
+        yield client
+
+
+def test_read_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'message': 'Welcome to the German-Context App!'}
