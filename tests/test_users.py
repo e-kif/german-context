6ec2066257967ea0d93e97234e805c0e7e492218
e-kif <e.kifiuk@gmail.com>
2025-03-19 16:54:57 +0100

    user not authenticated tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -14,3 +14,22 @@ def test_read_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'message': 'Welcome to the German-Context App!'}
+
+
+def test_user_non_authenticated(client):
+    endpoints = {
+        '/users/me': ['get', 'put', 'delete', 'patch'],
+        '/users/me/words': ['get', 'post'],
+        '/users/me/words/suggest': ['get'],
+        '/users/me/words/1': ['get', 'delete', 'patch', 'put'],
+        '/users/me/topics': ['get'],
+        '/users/me/topics/1': ['get', 'put', 'delete'],
+        '/user_cards/topic/1': ['get'],
+        '/user_cards/random': ['get'],
+        '/user_cards/update_info/1': ['get']
+    }
+    for endpoint, methods in endpoints.items():
+        for method in methods:
+            response = client.__getattribute__(method)(endpoint)
+            assert response.status_code == 401
+            assert response.json() == {'detail': 'Not authenticated'}
