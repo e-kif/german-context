afa5873b41cf1a92c59dfc6ca4439ffb12f03355
e-kif <e.kifiuk@gmail.com>
2025-03-22 08:28:10 +0100

    refactor: fixture moved to conftest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,15 +1,3 @@
-import pytest
-from fastapi.testclient import TestClient
-
-from main import app
-
-
-@pytest.fixture(scope='module')
-def client():
-    with TestClient(app) as client:
-        yield client
-
-
 def test_read_home(client):
     response = client.get('/')
     assert response.status_code == 200
